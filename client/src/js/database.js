24925da113c2775003f7775ad60079a4d33c863a
yinwu173 <yin.wu173@gmail.com>
2024-11-11 19:31:03 -0800

    added login to get all the contents from the database in the database.js file, made sure to catch any potential errors.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -40,6 +40,30 @@ export const putDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log('GET from the database');
+
+    // Create a connection to the database database and version we want to use
+    const jateDb = await openDB('content', 1);
+
+    // Create a new transaction and specify the database and data privileges
+    const tx = jateDb.transaction('jate', 'readonly');
+
+    // Open up the desired object store
+    const store = tx.objectStore('jate');
+
+    // Use the .getAll() method to get all data in the database
+    const request = store.getAll();
+
+    // Get confirmation of the request
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+    
+  } catch (error) {
+    console.error('Error saving data to the database', error);
+  };
+};
 
 initdb();
