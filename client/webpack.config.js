317cf3fa1531fd7e33e5465857c4255b32796319
yinwu173 <yin.wu173@gmail.com>
2024-11-13 22:22:32 -0800

    fixed path error in webpack.config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = () => {
       // Add service worker using webbox
       new InjectManifest({
         swSrc: './src-sw.js', 
-        swDest: '/src-sw.js', 
+        swDest: 'src-sw.js', 
       }),
 
       // Creates a manifest.json file.
